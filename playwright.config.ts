384da1e03739d771b5921fef295f76f4f53ece74
yurkovychv <vasyl.yurkovych@percona.com>
2024-03-06 12:13:03 +0200

    tweak config

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,11 +4,11 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   workers: process.env.CI ? 1 : undefined,
-  timeout: 90_000,
+  timeout: 60_000,
   expect: {
-    timeout: 10_000,
+    timeout: 15_000,
   },
   reporter: [
       ['html', { open: 'never' }],
